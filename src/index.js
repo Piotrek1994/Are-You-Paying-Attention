85c1257363f41a308a16a95911ddf574c1192212
unknown <pprus94@gmail.com>
2023-03-13 11:37:14 +0100

    updateSkyColor and updateGrassColor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
-    title: "Are You Paying Attention?",
-    icon: 'smiley',
-    category: "common",
-    edit: function(){
-        return (
-            <>
-             <div>Hello this is pharagraph</div>
-             <h4>okey</h4>
-            </>
-            )
-         
-    },
-    save: function(){
-        return wp.element.createElement("h1", null, "Hello, this is front-end")
+wp.blocks.registerBlockType('ourplugin/are-you-paying-attention', {
+	title: 'Are You Paying Attention?',
+	icon: 'smiley',
+	category: 'common',
+	attributes: {
+		skyColor: { type: 'string' },
+		grassColor: { type: 'string'}
+	},
+	edit: function (props) {
+		function updateSkyColor(event) {
+			props.setAttributes({ skyColor: event.target.value })
+		}
+		function updateGrassColor(event) {
+			props.setAttributes({ grassColor: event.target.value })
+		}
 
-    }
+		return (
+			<>
+				<input type='text' placeholder='sky color' name='' id='' onChange={updateSkyColor} value={props.attributes.skyColor}/>
+				<input type='text' placeholder='grass color' name='' id='' onChange={updateGrassColor} value={props.attributes.grassColor}/>
+			</>
+		)
+	},
+	save: function (props) {
+		return (
+			<p>
+				Today sky is <span className="skyColor">{props.attributes.skyColor}</span> and grass is <span className="grassColor">{props.attributes.grassColor}</span>.
+			</p>
+		)
+	},
 })
